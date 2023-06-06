f371ebd7513a8cfad801ef5a2a87760cf36d63b0
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-06-06 22:04:45 +0800

    Add: if statement | I/O

--- Program.cs
+++ Program.cs
@@ -6,17 +6,49 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // See https://aka.ms/new-console-template for more information
-            Console.WriteLine("Hello, World\nEnter your name: ");
+            // ++++++++++++> if statement <++++++++++++++++
+            // int age = 15;
+            // string status;
 
-            string test = Console.ReadLine();
-            Console.WriteLine($"[{test}]");
+            // if (age < 13)
+            //     status = "Child";
+            // else if (age < 20)
+            //     status = "Teen";
+            // else if (age < 60)
+            //     status = "Adult";
+            // else
+            //     status = "Old";
 
-            string str = "Hello";
-            Console.WriteLine($"[{str}]");
+            // char message = status == "Teen" ? '1' : '0';
+
+            // Console.WriteLine("status: {0} | message: {1}", status, message);
+
+            // ++++++++++++> I/O <++++++++++++++++
+            // Console.WriteLine("What is your name?");
+            // Console.Write("Type you first name: ");
+            // string? first_name;
+            // first_name = Console.ReadLine();
+
+            // Console.Write("Type you last name: ");
+            // string? last_name = Console.ReadLine();
+
+            // Console.WriteLine($"Hello, {first_name} {last_name}");
+            // Console.ReadLine();
+
+            // int x = 1;
+            // int y = x + 31;
+            // Console.WriteLine(y + "123");
 
 
+            // ++++++++++++>  <++++++++++++++++
+            // // See https://aka.ms/new-console-template for more information
+            // Console.WriteLine("Hello, World\nEnter your name: ");
 
+            // string? test = Console.ReadLine();
+            // Console.WriteLine($"[{test}]");
+
+            // string str = "Hello";
+            // Console.WriteLine($"[{str}]");
         }
     }
 }
