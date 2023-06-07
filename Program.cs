0bb803644ead1f429e08ac47e1ad6db076ff9cfc
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-06-07 21:03:54 +0800

    Add: array

--- Program.cs
+++ Program.cs
@@ -7,86 +7,110 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // ++++++++++++> array <++++++++++++++++
-            int[] numbers = { 1, 3, 5, 7, 9 };
-            string[] weekDays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
-            numbers.ToList().ForEach(i => Console.WriteLine(i.ToString()));
-            weekDays.ToList().ForEach(i => Console.WriteLine(i));
+            // int[] numbers = new int[5];
+            // numbers[0] = 11;
+            // numbers[1] = 12;
+            // numbers[2] = 13;
+            // numbers[3] = 14;
+            // numbers[4] = 15;
 
-            // ++++++++++++> loop <++++++++++++++++
-            // int i = 0;
-            // do
+            // numbers.ToList().ForEach(index,i => Console.WriteLine($"{i.ToString()} - {index}"));
+
+            // string[] words = { "a", "b", "c", "d" };
+            // char[] letters = new char[4] { 'a', 'b', 'c', 'd' };
+
+            // string letters = "qwnrcqnwbrjcn12n1r";
+            // char[] char_letters = letters.ToCharArray();
+            
+            // char_letters.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+
+            // foreach (char item in char_letters)
             // {
-            //     Console.Write("Input: ");
-            //     string? name;
-            //     name = Console.ReadLine();
-            //     Console.WriteLine($"{i}: {name}");
-            //     if (i == 10) break;
-            //     i++;
-            // } while (true);
-
-            // int i = 0;
-            // while (i < 10)
-            // {
-            //     Console.Write("Input: ");
-            //     string? name;
-            //     name = Console.ReadLine();
-            //     Console.WriteLine($"{i}: {name}");
-            //     i++;
+            //     Console.WriteLine(item);
             // }
 
-            // for (int i = 0; i < 10; i++)
-            // {
-
-            //     Console.Write("Input: ");
-            //     string? name;
-            //     name = Console.ReadLine();
-            //     Console.WriteLine($"{i}: {name}");
-            // }
-
-            // ++++++++++++> if statement <++++++++++++++++
-            // int age = 15;
-            // string status;
-
-            // if (age < 13)
-            //     status = "Child";
-            // else if (age < 20)
-            //     status = "Teen";
-            // else if (age < 60)
-            //     status = "Adult";
-            // else
-            //     status = "Old";
-
-            // char message = status == "Teen" ? '1' : '0';
-
-            // Console.WriteLine("status: {0} | message: {1}", status, message);
-
-            // ++++++++++++> I/O <++++++++++++++++
-            // Console.WriteLine("What is your name?");
-            // Console.Write("Type you first name: ");
-            // string? first_name;
-            // first_name = Console.ReadLine();
-
-            // Console.Write("Type you last name: ");
-            // string? last_name = Console.ReadLine();
-
-            // Console.WriteLine($"Hello, {first_name} {last_name}");
-            // Console.ReadLine();
-
-            // int x = 1;
-            // int y = x + 31;
-            // Console.WriteLine(y + "123");
-
-
-            // ++++++++++++>  <++++++++++++++++
-            // // See https://aka.ms/new-console-template for more information
-            // Console.WriteLine("Hello, World\nEnter your name: ");
-
-            // string? test = Console.ReadLine();
-            // Console.WriteLine($"[{test}]");
-
-            // string str = "Hello";
-            // Console.WriteLine($"[{str}]");
         }
     }
 }
+
+// int[] numbers = { 1, 3, 5, 7, 9 };
+// string[] weekDays = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+// numbers.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+// weekDays.ToList().ForEach(i => Console.WriteLine(i));
+
+// ++++++++++++> loop <++++++++++++++++
+// int i = 0;
+// do
+// {
+//     Console.Write("Input: ");
+//     string? name;
+//     name = Console.ReadLine();
+//     Console.WriteLine($"{i}: {name}");
+//     if (i == 10) break;
+//     i++;
+// } while (true);
+
+// int i = 0;
+// while (i < 10)
+// {
+//     Console.Write("Input: ");
+//     string? name;
+//     name = Console.ReadLine();
+//     Console.WriteLine($"{i}: {name}");
+//     i++;
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+
+//     Console.Write("Input: ");
+//     string? name;
+//     name = Console.ReadLine();
+//     Console.WriteLine($"{i}: {name}");
+// }
+
+// ++++++++++++> if statement <++++++++++++++++
+// int age = 15;
+// string status;
+
+// if (age < 13)
+//     status = "Child";
+// else if (age < 20)
+//     status = "Teen";
+// else if (age < 60)
+//     status = "Adult";
+// else
+//     status = "Old";
+
+// char message = status == "Teen" ? '1' : '0';
+
+// Console.WriteLine("status: {0} | message: {1}", status, message);
+
+// ++++++++++++> I/O <++++++++++++++++
+// Console.WriteLine("What is your name?");
+// Console.Write("Type you first name: ");
+// string? first_name;
+// first_name = Console.ReadLine();
+
+// Console.Write("Type you last name: ");
+// string? last_name = Console.ReadLine();
+
+// Console.WriteLine($"Hello, {first_name} {last_name}");
+// Console.ReadLine();
+
+// int x = 1;
+// int y = x + 31;
+// Console.WriteLine(y + "123");
+
+
+// ++++++++++++>  <++++++++++++++++
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World\nEnter your name: ");
+
+// string? test = Console.ReadLine();
+// Console.WriteLine($"[{test}]");
+
+// string str = "Hello";
+// Console.WriteLine($"[{str}]");
